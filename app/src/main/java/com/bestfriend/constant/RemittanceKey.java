54899b236827f1097be58fe3d54485c2a411c931
Jeffrey <jeffre.concha@mobileoptima.com>
2017-10-13 16:25:38 +0800

    Additional format for different messages

--- app/src/main/java/com/bestfriend/constant/RemittanceKey.java
+++ app/src/main/java/com/bestfriend/constant/RemittanceKey.java
@@ -8,7 +8,7 @@ public class RemittanceKey {
 	public static final String RECEIVE_SP_4 = "was added to your account";
 	public static final String TRANSFER_SP_1 = "Also deducted";
 	public static final String TRANSFER_SP_2 = "was deducted from";
-	public static final String TRANSFER_SP_3 = "Sa next msg,i-type ang customer &receiver cellphone#";
+	public static final String TRANSFER_SP_3 = "Sa next msg,i-type ang customer";
 	public static final String TRANSFER_SM = "have transferred";
 	public static final String BALANCE = "2/2 Bal:";
 	public static final String SENDER_SP = "SmartPadala";
